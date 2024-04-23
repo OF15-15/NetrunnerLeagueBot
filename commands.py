59ee3cc9b6488de90a03618409d8ff8994a720cc
ich <net.a@gmx.de>
2024-04-24 00:14:13 +0200

    auth updated

--- commands.py
+++ commands.py
@@ -6,18 +6,13 @@ import sqlite3
 import functools
 import json as js
 
-command_list = []
-with open("data.json") as f:
-    guilds_data = js.load(f)
-
-
 def command(name, description, perms):
     def decorator(func):
         @functools.wraps(func)
         async def wrapper(ia, *args, **kwargs):
             if perms == "everyone":
                 pass
-            elif auth_admin(ia.guild, ia.user):
+            elif auth_admin(ia.guild_id, ia.user.id):
                 pass
             else:
                 return await ia.response.send_message(f"Du darfst diesen Befehl nicht benutzen", ephemeral=True)
@@ -27,20 +22,21 @@ def command(name, description, perms):
         return wrapper
     return decorator
 
-"""
-@command("echo", "Echo the string put in", ["admin"], ["admin"])
-async def echo(ia, content: str):
-    await ia.response.send_message(content)
-"""
+command_list = []
 
 db: sqlite3.Connection
 cursor: sqlite3.Cursor
 
 
 def auth_admin(guild, user):
-    cursor.execute('''SELECT admin_id FROM admins WHERE guild_id = ? AND admin_id=?''', (guild, user))
+    cursor.execute('''SELECT * FROM admins WHERE guild_id = ? AND user_id=?''', (guild, user))
     owner = cursor.fetchone()
     db.commit()
     if owner is None:
         return False
     return True
+
+
+@command("echo", "Echo the string put in", "admin")
+async def echo(ia, content: str):
+    await ia.response.send_message(content)
