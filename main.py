eef5d8f14f5a36155b757bfdf47899779042b0c7
ich <net.a@gmx.de>
2024-04-24 00:14:38 +0200

    updated everything to new database

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ import json as js
 with open("token.txt") as f:
     token = f.read()
 
-# all the data
-with open("data.json") as f:
-    guilds_data = js.load(f)
-
 # create client object and a command tree for /-commands
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
@@ -23,15 +19,21 @@ cursor = db.cursor()
 commands.db = db
 commands.cursor = cursor
 
+cursor.execute('''SELECT * FROM guilds''')
+guilds = cursor.fetchall()
+db.commit()
+
+guild_ids = [g[0] for g in guilds]
 
 for cmd in commands.command_list:
-    tree.add_command(cmd, guilds=[discord.Object(id=g["guild_id"]) for g in guilds_data])
+    tree.add_command(cmd, guilds=[discord.Object(id=g) for g in guild_ids])
 
 
 @client.event
 async def on_ready():
-    for guild in guilds_data:
-        await tree.sync(guild=discord.Object(id=guild["guild_id"]))
+    for guild_id in guild_ids:
+        await tree.sync(guild=discord.Object(id=guild_id))
     print("Ready!")
+    print(f"Logged in on {', '.join([g[1] for g in guilds])}")
 
 client.run(token)
