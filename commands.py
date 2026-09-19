736067d1ffec73049c0f4c4d01adca1458299024
ich <git@of15-15.de>
2026-09-19 19:22:19 +0200

    fix

--- commands.py
+++ commands.py
@@ -497,12 +497,12 @@ async def remove_tournaments(ia):
 
 @command("extension", "give an extension", "admin")
 async def extension(ia, table: int, duration: int):
-    cursor.execute('''SELECT endtime, player1, player2 FROM game_timers WHERE tournament_id=?''', (table,))
+    cursor.execute('''SELECT end_time, player1, player2 FROM game_timers WHERE tournament_id=?''', (table,))
     db.commit()
     row = cursor.fetchone()
     if row is None:
         return await ia.response.send_message(f"No game found for table {table}.", ephemeral=True)
-    cursor.execute('''UPDATE game_timers SET endtime=? WHERE tournament_id=?''', (row[0]+duration, table))
+    cursor.execute('''UPDATE game_timers SET end_time=? WHERE tournament_id=?''', (row[0]+duration, table))
     db.commit()
     return await ia.response.send_message(f"Extension table {table}: {row[1]} - {row2} - new endtime <t:{row[0]+duration}:t> <t:{row[0]+duration}:R>")
 
